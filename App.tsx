64ff283838a43f8527da6e30d9cbb6a92085104b
Marcos Duarte <marcosduarte1994@gmail.com>
2023-01-04 00:07:43 -0300

    feat: create configuration fonts roboto

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
+import { ActivityIndicator } from'react-native'
 import { ThemeProvider } from 'styled-components'
+import { 
+  useFonts, 
+  Roboto_400Regular, 
+  Roboto_700Bold 
+} from '@expo-google-fonts/roboto'
+
+
 import theme from './src/theme'
 import { Groups } from "@screens/groups";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
