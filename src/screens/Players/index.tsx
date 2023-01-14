da503405eaaa57656063d87a7571a62e70cfa05e
Marcos Duarte <marcosduarte1994@gmail.com>
2023-01-13 22:29:59 -0300

    feat: create component for number players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+
 import { ButtonIcons } from '@components/ButtonIcon'
 import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
-import { Container, Form } from './styles'
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles'
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState([])
   return (
     <Container>
       <Header showBackButton />
@@ -14,17 +20,37 @@ export function Players() {
         title="Nome da Turma"
         subtitle="Adicione a galera e separe os times"
       />
+
       <Form>
         <Input 
           placeholder="Nome da pessoa"
           autoCorrect={false}
         />
-        <ButtonIcons icon="add" />
+
+        <ButtonIcons 
+          icon="add" 
+        />
       </Form>
-      <Filter 
-        title="Time A" 
-        isActive
-      />
+
+      <HeaderList>
+        <FlatList 
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter 
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)} 
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
+
     </Container>
   )
 }
