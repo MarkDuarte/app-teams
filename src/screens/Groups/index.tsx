3fa9f347d55faab926ad79dcd2f658d40e8ab817
Marcos Duarte <marcosduarte1994@gmail.com>
2023-01-14 00:06:48 -0300

    refactor: refactor in component of players

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -10,7 +10,7 @@ import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button'
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Amigos'])
+  const [groups, setGroups] = useState<string[]>([])
   return (
     <Container>
       <Header />
@@ -32,6 +32,7 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma ?" />
         )}
+        showsVerticalScrollIndicator={false}
       />
 
       <Button 
