8209ab4893e18ada7a2b341024d00c361fa7bbf2
Marcos Duarte <marcosduarte1994@gmail.com>
2023-01-19 17:28:22 -0300

    feat: new navigation for teams

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -13,6 +13,12 @@ import { Button } from '@components/Button'
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
 
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
+
   return (
     <Container>
       <Header />
@@ -39,6 +45,7 @@ export function Groups() {
 
       <Button 
         title="Criar nova turma"
+        onPress={handleNewGroup}
       />
     </Container>
   );
