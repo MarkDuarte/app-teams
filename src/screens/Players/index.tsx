874bf8e4500bc3893f060c12373717398bf15fc6
Marcos Duarte <marcosduarte1994@gmail.com>
2023-01-20 23:58:03 -0300

    feat: update name team

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { FlatList } from 'react-native'
+import { useRoute } from '@react-navigation/native'
 
 import { ButtonIcons } from '@components/ButtonIcon'
 import { Filter } from '@components/Filter'
@@ -12,15 +13,23 @@ import { ListEmpty } from '@components/ListEmpty'
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
 import { Button } from '@components/Button'
 
+interface RouteParams {
+  group: string
+}
+
 export function Players() {
   const [team, setTeam] = useState('Time A')
   const [players, setPlayers] = useState([])
+
+  const route = useRoute()
+  const { group } = route.params as RouteParams
+
   return (
     <Container>
       <Header showBackButton />
 
       <Highlight 
-        title="Nome da Turma"
+        title={group}
         subtitle="Adicione a galera e separe os times"
       />
 
