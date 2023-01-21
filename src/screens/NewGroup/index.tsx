bfb792b1d0053fa7482dff98bc5ae48f6c05cbea
Marcos Duarte <marcosduarte1994@gmail.com>
2023-01-21 01:29:35 -0300

    chore: identifying the focus on the screen

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+import { groupCreate } from '@storage/group/groupCreate'
+
 import { Button } from '@components/Button'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
-import { useNavigation } from '@react-navigation/native'
+
 import { Container, Content, Icon } from './styles'
 
 export function NewGroup() {
@@ -11,8 +15,13 @@ export function NewGroup() {
 
   const navigation = useNavigation()
 
-  function handleNew() {
-    navigation.navigate('players', { group })
+  async function handleNew() {
+    try {
+      await groupCreate(group)
+      navigation.navigate('players', { group })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
